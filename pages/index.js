dcc5896409a909f551a0873a8794608a97185715
athena-service-account <pizzaburgergordon@gmail.com>
2025-07-07 19:37:30 -0400

    Athena: • "Create a new index.html page with a blue backgr...

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Home</title>
+      </Head>
+      <h1>Centered Text</h1>
+    </div>
+  );
+}
+
+export default Home;
